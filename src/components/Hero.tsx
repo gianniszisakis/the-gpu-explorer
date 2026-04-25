0860e938972b6d3e27f996e78c681bd92ef6bf4b
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2026-04-25 23:04:29 +0300

    feat: Create hero component.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export function Hero() {
+  return (
+    <div className="relative w-full h-[220px] overflow-hidden">
+      <img
+        src="/gpu-explorer-banner.jpg"
+        alt="hero"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
